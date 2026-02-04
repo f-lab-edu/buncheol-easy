0074e64bf0f67d1dcc378ec1159f272c30dddb51
ChaeAg <dydkglel123@gmail.com>
2026-02-04 12:35:44 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/buncheoleasy/BuncheoleasyApplication.java
@@ -0,0 +1,11 @@
+package buncheoleasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuncheoleasyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BuncheoleasyApplication.class, args);
+	}
+}
---
+++ src/test/java/buncheoleasy/BuncheoleasyApplicationTests.java
@@ -0,0 +1,13 @@
+package buncheoleasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuncheoleasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

940aae0720c058e08694b365da298fefc616ddf8
ChaeAg <dydkglel123@gmail.com>
2026-02-13 13:56:58 +0900

    fix: User 검증 길이 에러 메시지 기준 정합성 수정

--- src/main/java/buncheoleasy/global/exception/domain/ErrorCode.java
+++ src/main/java/buncheoleasy/global/exception/domain/ErrorCode.java
@@ -13,15 +13,15 @@ public enum ErrorCode {
      * USR - 유저 관련 에러
      */
     USER_SOCIAL_ID_REQUIRED("USR-001", "소셜 고유 ID는 필수입니다.", HttpStatus.BAD_REQUEST),
-    USER_SOCIAL_ID_LENGTH_INVALID("USR-002", "소셜 고유 ID는 64자 이하여야 합니다.", HttpStatus.BAD_REQUEST),
+    USER_SOCIAL_ID_LENGTH_INVALID("USR-002", "소셜 고유 ID는 100자 이하여야 합니다.", HttpStatus.BAD_REQUEST),
     USER_SOCIAL_ID_FORMAT_INVALID("USR-003", "소셜 고유 ID는 영문자, 숫자, 언더스코어(_), 하이픈(-)만 사용 가능합니다.", HttpStatus.BAD_REQUEST),
 
     USER_NICKNAME_REQUIRED("USR-004", "닉네임은 필수입니다.", HttpStatus.BAD_REQUEST),
-    USER_NICKNAME_LENGTH_INVALID("USR-005", "닉네임은 1자 이상 10자 이하여야 합니다.", HttpStatus.BAD_REQUEST),
+    USER_NICKNAME_LENGTH_INVALID("USR-005", "닉네임은 1자 이상 20자 이하여야 합니다.", HttpStatus.BAD_REQUEST),
     USER_NICKNAME_FORMAT_INVALID("USR-006", "닉네임은 한글, 영문자, 숫자만 사용 가능합니다.", HttpStatus.BAD_REQUEST),
 
     USER_EMAIL_REQUIRED("USR-007", "이메일은 필수입니다.", HttpStatus.BAD_REQUEST),
-    USER_EMAIL_LENGTH_INVALID("USR-008", "이메일은 255자 이하여야 합니다.", HttpStatus.BAD_REQUEST),
+    USER_EMAIL_LENGTH_INVALID("USR-008", "이메일은 320자 이하여야 합니다.", HttpStatus.BAD_REQUEST),
     USER_EMAIL_FORMAT_INVALID("USR-009", "올바른 이메일 형식이 아닙니다.", HttpStatus.BAD_REQUEST),
 
     USER_PHONE_NUMBER_REQUIRED("USR-010", "전화번호는 필수입니다.", HttpStatus.BAD_REQUEST),
